4d0869dfec8037907f6ff7a08cef830520ad36fc
AbhinavRAWAT <95606526+Ahhsdhhd@users.noreply.github.com>
2022-10-13 14:26:23 +0530

    first commit

--- app.js
+++ app.js
@@ -123,7 +123,8 @@ app.get('/',(req,res)=>{
 
 
 //delete this local host
-const server = 
-app.listen(port,()=> console.log('server conn : ' + port));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 //create port
